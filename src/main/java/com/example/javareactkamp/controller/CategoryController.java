9c1c924e6c571e24b54b5a6f47ca18d9be399ea7
ShmsErl <erol.sehmus1@gmail.com>
2024-01-07 17:14:34 +0300

    React ile arayüz tasarımı gerçekleştirildi.

--- src/main/java/com/example/javareactkamp/controller/CategoryController.java
+++ src/main/java/com/example/javareactkamp/controller/CategoryController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/categories")
 @AllArgsConstructor
-@CrossOrigin
+
 public class CategoryController {
 
     private final CategoryService categoryService;
--- src/main/java/com/example/javareactkamp/controller/ProductController.java
+++ src/main/java/com/example/javareactkamp/controller/ProductController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 @AllArgsConstructor
-@CrossOrigin
+
 public class ProductController {
 
     private final ProductService productService;
